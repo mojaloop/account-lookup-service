5c34b237b209b50b86438ce45c0f95eb588a9db4
Rajiv Mothilal <rajivmothilal@gmail.com>
2019-03-15 13:03:05 +0200

    updates to tables design

---
+++ seeds/partyIdType.js
@@ -0,0 +1,68 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+
+ * Rajiv Mothilal <rajiv.mothilal@modusbox.com>
+
+ --------------
+ ******/
+'use strict'
+
+const partyIdTypes = [
+  {
+    'name': 'MSISDN',
+    'description': 'A MSISDN (Mobile Station International Subscriber Directory Number, that is, the phone number) is used as reference to a participant. The MSISDN identifier should be in international format according to the ITU-T E.164 standard. Optionally, the MSISDN may be prefixed by a single plus sign, indicating the international prefix.'
+  },
+  {
+    'name': 'EMAIL',
+    'description': 'An email is used as reference to a participant. The format of the email should be according to the informational RFC 3696.'
+  },
+  {
+    'name': 'PERSONAL_ID',
+    'description': 'A personal identifier is used as reference to a participant. Examples of personal identification are passport number, birth certificate number, and national registration number. The identifier number is added in the PartyIdentifier element. The personal identifier type is added in the PartySubIdOrType element.'
+  },
+  {
+    'name': 'BUSINESS',
+    'description': 'A specific Business (for example, an organization or a company) is used as reference to a participant. The BUSINESS identifier can be in any format. To make a transaction connected to a specific username or bill number in a Business, the PartySubIdOrType element should be used.'
+  },
+  {
+    'name': 'DEVICE',
+    'description': 'A specific device (for example, a POS or ATM) ID connected to a specific business or organization is used as reference to a Party. For referencing a specific device under a specific business or organization, use the PartySubIdOrType element.'
+  },
+  {
+    'name': 'ACCOUNT_ID',
+    'description': 'A bank account number or FSP account ID should be used as reference to a participant. The ACCOUNT_ID identifier can be in any format, as formats can greatly differ depending on country and FSP.'
+  },
+  {
+    'name': 'IBAN',
+    'description': 'A bank account number or FSP account ID is used as reference to a participant. The IBAN identifier can consist of up to 34 alphanumeric characters and should be entered without whitespace.'
+  },
+  {
+    'name': 'ALIAS',
+    'description': 'An alias is used as reference to a participant. The alias should be created in the FSP as an alternative reference to an account owner. Another example of an alias is a username in the FSP system. The ALIAS identifier can be in any format. It is also possible to use the PartySubIdOrType element for identifying an account under an Alias defined by the PartyIdentifier.'
+  }
+]
+
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('partyIdType').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('partyIdType').insert(partyIdTypes)
+    })
+}
