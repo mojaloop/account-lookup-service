c0d757122335448c5f68b002aca67fc84649f233
Rajiv Mothilal <rajivmothilal@gmail.com>
2019-05-24 09:20:21 +0200

    added isDefault to primary key

--- migrations/09_oracleEndpoint-indexes.js
+++ migrations/09_oracleEndpoint-indexes.js
@@ -29,7 +29,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.table('oracleEndpoint', (t) => {
     t.index('partyIdTypeId')
     t.index('endpointTypeId')
-    t.unique(['partyIdTypeId', 'currencyId'])
+    t.unique(['partyIdTypeId', 'currencyId', 'isDefault'])
   })
 }
 
