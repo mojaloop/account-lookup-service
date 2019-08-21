3afbee78ce025d30fcb4822cea2491004bf1ad4c
msk- <mattkingston@gmail.com>
2019-08-21 15:50:22 +0100

    Adding new required files representing domain and models- should have been in previous commit

---
+++ src/domain/index.js
@@ -0,0 +1,10 @@
+
+const oracle = require('./oracle')
+const participants = require('./participants')
+const parties = require('./parties')
+
+module.exports = {
+    oracle,
+    participants,
+    parties
+}
---
+++ src/models/index.js
@@ -0,0 +1,14 @@
+
+const currency = require('./currency')
+const endpointType = require('./endpointType')
+const oracle = require('./oracle')
+const participantEndpoint = require('./participantEndpoint/facade')
+const partyIdType = require('./partyIdType')
+
+module.exports = {
+    currency,
+    endpointType,
+    oracle,
+    participantEndpoint,
+    partyIdType
+}
