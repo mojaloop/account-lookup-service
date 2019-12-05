121d53810946444801e4c8ef59b03747db722aba
Steven Oderayi <oderayi@gmail.com>
2019-12-05 10:39:29 +0100

    Restore configurations for docker-compose

--- docker/wait-for/wait-for-account-lookup-service.sh
+++ docker/wait-for/wait-for-account-lookup-service.sh
@@ -5,6 +5,6 @@ echo "** STARTUP - Checking for Account Lookup Service..."
 source /opt/wait-for/wait-for.env
 
 sh /opt/wait-for/wait-for-mysql-als.sh
-# sh /opt/wait-for/wait-for-mysql-central-ledger.sh
+sh /opt/wait-for/wait-for-mysql-central-ledger.sh
 
 echo "** STARTUP - Account Lookup Service successful!"
