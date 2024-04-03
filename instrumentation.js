e4873ad4a1676d46a948c7fb99b71f9b4e8da3b6
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2024-04-03 00:46:38 -0600

    change otlp from http to grpc

--- instrumentation.js
+++ instrumentation.js
@@ -1,21 +1,19 @@
-const { BasicTracerProvider, BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base')
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http')
+const { BasicTracerProvider, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base')
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc')
 
 const collectorOptions = {
-  url: 'tempo-grafana-tempo-distributor.monitoring.cluster.svc.local:55681', // url is optional and can be omitted - default is http://localhost:4318/v1/traces
-  headers: {
-    foo: 'bar'
-  }, // an optional object containing custom headers to be sent with each request will only work with http
-  concurrencyLimit: 10 // an optional limit on pending requests
+  // url is optional and can be omitted - default is http://localhost:4317
+  // Unix domain sockets are also supported: 'unix:///path/to/socket.sock'
+  url: 'tempo-grafana-tempo-distributor.monitoring.cluster.svc.local:4317'
 }
 
 const provider = new BasicTracerProvider()
 const exporter = new OTLPTraceExporter(collectorOptions)
-provider.addSpanProcessor(new BatchSpanProcessor(exporter, {
-  // The maximum queue size. After the size is reached spans are dropped.
-  maxQueueSize: 1000,
-  // The interval between two consecutive exports
-  scheduledDelayMillis: 30000
-}))
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter))
+
+const signals = ['SIGINT', 'SIGTERM']
 
 provider.register()
+signals.forEach(signal => {
+  process.on(signal, () => provider.shutdown().catch(console.error))
+})
