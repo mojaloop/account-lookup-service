d12c75c6f68d21dd7312b68afb7ba1db183263f4
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2024-04-03 09:25:45 -0600

    fix ref to tempo naming

--- instrumentation.js
+++ instrumentation.js
@@ -2,9 +2,7 @@ const { BasicTracerProvider, SimpleSpanProcessor } = require('@opentelemetry/sdk
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc')
 
 const collectorOptions = {
-  // url is optional and can be omitted - default is http://localhost:4317
-  // Unix domain sockets are also supported: 'unix:///path/to/socket.sock'
-  url: 'tempo-grafana-tempo-distributor.monitoring.cluster.svc.local:4317'
+  url: 'tempo-grafana-tempo-distributor.monitoring.svc.cluster.local:4317'
 }
 
 const provider = new BasicTracerProvider()
