c589c45a28501c3b1d03f3c025f0c39e1f399a7b
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2024-04-04 11:06:40 -0600

    configure proper exporters

--- instrumentation.js
+++ instrumentation.js
@@ -1,15 +1,22 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node')
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node')
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http')
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http')
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node')
-const {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter
-} = require('@opentelemetry/sdk-metrics')
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics')
+
+const hostName = process.env.OTEL_EXPORTER_OTLP_ENDPOINT || 'tempo-distributor'
 
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: `${hostName}:14268/v1/traces`,
+    headers: {}
+  }),
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter()
+    exporter: new OTLPMetricExporter({
+      url: `http://${hostName}:14268/api/v1/metrics`,
+      headers: {}, // an optional object containing custom headers to be sent with each request
+      concurrencyLimit: 1 // an optional limit on pending requests
+    })
   }),
   instrumentations: [getNodeAutoInstrumentations()]
 })
