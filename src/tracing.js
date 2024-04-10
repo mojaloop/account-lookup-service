470b341f340752a7252d8b9e04b6091083cf26bd
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2024-04-10 08:35:26 -0600

    hardcode tempo URL

--- src/tracing.js
+++ src/tracing.js
@@ -2,11 +2,12 @@ const opentelemetry = require('@opentelemetry/sdk-node')
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node')
 const { HapiInstrumentation } = require('@opentelemetry/instrumentation-hapi')
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger')
-const Config = require('./lib/config')
+// const Config = require('./lib/config')
 
 const options = {
   tags: [], // optional
-  endpoint: Config.INSTRUMENTATION_METRICS_TEMPO_URL || 'http://localhost:14268/api/traces'
+  endpoint: 'http://tempo-grafana-tempo-distributor.monitoring.svc.cluster.local:14268/api/traces'
+  // endpoint: Config.INSTRUMENTATION_METRICS_TEMPO_URL || 'http://localhost:14268/api/traces'
 }
 const exporter = new JaegerExporter(options)
 
