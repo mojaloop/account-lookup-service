daf6134db95420e5ed9a5d15b97ac268333fee4f
Steven Oderayi <oderayi@gmail.com>
2024-07-28 16:30:11 +0100

    ci: update dc scripts

--- test/integration/env.sh
+++ test/integration/env.sh
@@ -1,9 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+get_root_dir() {
+  SOURCE="$0"
+  if [ -h "$SOURCE" ]; then
+    while [ -h "$SOURCE" ]; do
+      DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+      SOURCE="$(readlink "$SOURCE")"
+      [ "$SOURCE" != "/*" ] && SOURCE="$DIR/$SOURCE"
+    done
+  fi
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  echo "$DIR"
+}
 
 get_external_ip() {
-  if [ "$(uname)" == "Linux" ]; then
+  if [ "$(uname)" = "Linux" ]; then
     echo "$(ip addr show docker0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)"
   else
     # Need to find a way to support Windows here
@@ -12,13 +23,7 @@ get_external_ip() {
 }
 
 # set .env values
-set -a && . $DIR/.env && set +a
+set -a && .  $(get_root_dir)/test/integration/.env && set +a
 
 # set/override dynamic variables
 export REDIS_CLUSTER_ANNOUNCE_IP=$(get_external_ip)
-
-# if last command failed, exit
-if [ $? -ne 0 ]; then
-  echo "Failed to set REDIS_CLUSTER_ANNOUNCE_IP environment variable"
-  exit 1
-fi
