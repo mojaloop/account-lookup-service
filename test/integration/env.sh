56706f2b79c79d793cf4db9ecfc4848ce7dbdbb6
Steven Oderayi <oderayi@gmail.com>
2024-07-29 03:44:28 +0100

    ci: update env

--- test/integration/env.sh
+++ test/integration/env.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
 get_root_dir() {
-  SOURCE="$0"
-  if [ -h "$SOURCE" ]; then
-    while [ -h "$SOURCE" ]; do
-      DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
-      SOURCE="$(readlink "$SOURCE")"
-      [ "$SOURCE" != "/*" ] && SOURCE="$DIR/$SOURCE"
-    done
-  fi
-  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
-  echo "$DIR"
+  echo "$( dirname -- "$(readlink -f -- "$0")" )"
 }
 
 # Retrieve the external IP address of the host machine (on macOS)
 # or the IP address of the docker0 interface (on Linux)
-# to be used for the Redis cluster announce IP
 get_external_ip() {
   if [ "$(uname)" = "Linux" ]; then
     echo "$(ip addr show docker0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)"
