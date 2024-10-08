a6b95dbd3045d8856164e77ed7593733f409445f
Eugen Klymniuk <keramet.kh@gmail.com>
2024-10-08 17:06:18 +0100

    fix(csi-746): passed apiType to sendRequest (#495)
    * fix(csi-746): passed apiType to sendRequest
    
    * fix(csi-746): passed apiType to sendRequest
    
    * fix(csi-746): fixed int test
    
    * fix(csi-746): fixed int test
    
    * fix(csi-746): fixed int test
    
    * fix(csi-746): fixed int test
    
    * fix(csi-746): fixed int test

--- test/integration/domain/timeout/index.test.js
+++ test/integration/domain/timeout/index.test.js
@@ -4,9 +4,9 @@ const fixtures = require('../../../fixtures')
 const { ProxyApiClient } = require('../../../util')
 const config = require('../../../../src/lib/config')
 
-const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+const wait = (sec) => new Promise(resolve => setTimeout(resolve, sec * 1000))
 
-const CRON_TIMEOUT_MS = 10_000 // see TIMEXP
+const CRON_TIMEOUT_SEC = 10 // see TIMEXP
 
 describe('Timeout Handler', () => {
   const { type, proxyConfig } = config.PROXY_CACHE_CONFIG
@@ -43,13 +43,13 @@ describe('Timeout Handler', () => {
     const alsReq1 = fixtures.mockAlsRequestDto(PAYER_DFSP, PARTY_ID_TYPE, partyIds[0])
     const alsReq2 = fixtures.mockAlsRequestDto(PAYER_DFSP, PARTY_ID_TYPE, partyIds[1])
     const results = await Promise.all([
-      proxyCache.setSendToProxiesList(alsReq1, proxies),
-      proxyCache.setSendToProxiesList(alsReq2, proxies)
+      proxyCache.setSendToProxiesList(alsReq1, proxies, CRON_TIMEOUT_SEC),
+      proxyCache.setSendToProxiesList(alsReq2, proxies, CRON_TIMEOUT_SEC)
     ])
     expect(results.includes(false)).toBe(false)
 
     // wait for the timeout handler to process the keys
-    await wait(CRON_TIMEOUT_MS * 1.5)
+    await wait(CRON_TIMEOUT_SEC * 1.5)
 
     // check that the keys are no longer in redis
     const exists = await Promise.all(keys.map(key => proxyCache.redisClient.exists(key)))
@@ -58,11 +58,11 @@ describe('Timeout Handler', () => {
     // check that the callbacks are sent and received at the FSP
     // for test resilience, we will retry the history check a few times
     const retryMaxCount = 20
-    const retryInterval = 2000
+    const retryIntervalSec = 2
     let retryCount = 0
 
     while (history.length < 2 && retryCount < retryMaxCount) {
-      await wait(retryInterval)
+      await wait(retryIntervalSec)
       history = await proxyClient.getHistory()
       retryCount++
     }
