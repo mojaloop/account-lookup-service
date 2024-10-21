e22c7d43145caee731b6f92ef8e57a5ae34b1182
Kalin Krustev <kalin.krustev@gmail.com>
2024-10-21 09:47:14 +0000

    fix: github-advanced-security alerts

--- docker/mock-proxy/src/config.ts
+++ docker/mock-proxy/src/config.ts
@@ -1,13 +1,18 @@
 import process from 'node:process';
 
-console.log('process.env:', process.env);
-
 export const {
   PROXY_PORT = 14200,
   PROXY_NAME = 'proxyAB',
   CL_HOST = 'localhost',
   CL_PORT = 3001,
 } = process.env;
+
+console.log({
+  PROXY_PORT,
+  PROXY_NAME,
+  CL_HOST,
+  CL_PORT
+})
 
 export const ROUTES = {
   history: '/history',
