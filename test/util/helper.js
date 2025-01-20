aa8ffa37fa389fd84daeb0dee69d5f05694567d5
Kevin Leyow <kleyow@gmail.com>
2025-01-20 08:19:30 -0500

    chore: bump deps for date validation

--- test/util/helper.js
+++ test/util/helper.js
@@ -88,7 +88,7 @@ function defaultHeaders (destination, resource, source, version = '1.1', apiType
     accept: `application/vnd.interoperability${isoPart}.${resource}+json;version=${version}`,
     'fspiop-destination': destination || '',
     'content-type': `application/vnd.interoperability${isoPart}.${resource}+json;version=${version}`,
-    date: '2019-05-24 08:52:19',
+    date: new Date().toUTCString(),
     'fspiop-source': source,
     'fspiop-signature': '{"signature":"iU4GBXSfY8twZMj1zXX1CTe3LDO8Zvgui53icrriBxCUF_wltQmnjgWLWI4ZUEueVeOeTbDPBZazpBWYvBYpl5WJSUoXi14nVlangcsmu2vYkQUPmHtjOW-yb2ng6_aPfwd7oHLWrWzcsjTF-S4dW7GZRPHEbY_qCOhEwmmMOnE1FWF1OLvP0dM0r4y7FlnrZNhmuVIFhk_pMbEC44rtQmMFv4pm4EVGqmIm3eyXz0GkX8q_O1kGBoyIeV_P6RRcZ0nL6YUVMhPFSLJo6CIhL2zPm54Qdl2nVzDFWn_shVyV0Cl5vpcMJxJ--O_Zcbmpv6lxqDdygTC782Ob3CNMvg\\",\\"protectedHeader\\":\\"eyJhbGciOiJSUzI1NiIsIkZTUElPUC1VUkkiOiIvdHJhbnNmZXJzIiwiRlNQSU9QLUhUVFAtTWV0aG9kIjoiUE9TVCIsIkZTUElPUC1Tb3VyY2UiOiJPTUwiLCJGU1BJT1AtRGVzdGluYXRpb24iOiJNVE5Nb2JpbGVNb25leSIsIkRhdGUiOiIifQ"}'
   }
@@ -113,7 +113,7 @@ const getByTypeIdRequest = {
     accept: 'application/vnd.interoperability.participants+json;version=1',
     'fspiop-destination': payeefsp,
     'content-type': 'application/vnd.interoperability.participants+json;version=1.1',
-    date: '2019-05-24 08:52:19',
+    date: new Date().toUTCString(),
     'fspiop-source': payerfsp
   },
   method: 'get'
@@ -129,7 +129,7 @@ const getByTypeIdRequestError = {
     accept: 'application/vnd.interoperability.participants+json;version=1',
     'fspiop-destination': payeefsp,
     'content-type': 'application/vnd.interoperability.participants+json;version=1.1',
-    date: '2019-05-24 08:52:19',
+    date: new Date().toUTCString(),
     'fspiop-source': payerfsp
   },
   method: 'get'
@@ -145,7 +145,7 @@ const putByTypeIdRequest = {
     accept: 'application/vnd.interoperability.participants+json;version=1',
     'fspiop-destination': payeefsp,
     'content-type': 'application/vnd.interoperability.participants+json;version=1.1',
-    date: '2019-05-24 08:52:19',
+    date: new Date().toUTCString(),
     'fspiop-source': payerfsp
   },
   method: 'put'
@@ -163,7 +163,7 @@ const getByTypeIdCurrencyRequest = {
     accept: 'application/vnd.interoperability.participants+json;version=1',
     'fspiop-destination': payeefsp,
     'content-type': 'application/vnd.interoperability.participants+json;version=1.1',
-    date: '2019-05-24 08:52:19',
+    date: new Date().toUTCString(),
     'fspiop-source': payerfsp
   },
   method: 'get'
@@ -181,7 +181,7 @@ const postByTypeIdCurrencyRequest = {
     accept: 'application/vnd.interoperability.participants+json;version=1',
     'fspiop-destination': payeefsp,
     'content-type': 'application/vnd.interoperability.participants+json;version=1.1',
-    date: '2019-05-24 08:52:19',
+    date: new Date().toUTCString(),
     'fspiop-source': payerfsp
   },
   method: 'post'
