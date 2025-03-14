f614b9d70c68b20da9ee194ef40a60e0aa9878b4
Kevin Leyow <kleyow@gmail.com>
2025-03-14 00:15:56 -0500

    chore: remove proxy map onboarding

--- test/unit/api/health.test.js
+++ test/unit/api/health.test.js
@@ -116,7 +116,6 @@ describe('/health', () => {
     // Arrange
     Config.PROXY_CACHE_CONFIG.enabled = true
     Config.API_PORT = await getPort()
-    Config.proxyMap = { proxied: 'proxy' }
     let serverWithProxy
     try {
       Metrics.getDefaultRegister().clear()
