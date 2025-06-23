633457e837eb25a4252fbeb71259edbfe0bff67b
geka.evk <geka.evk@gmail.com>
2025-06-23 17:02:11 +0100

    fix(csi-1592): fixed test-integration.sh

--- test/scripts/test-integration.sh
+++ test/scripts/test-integration.sh
@@ -6,8 +6,9 @@ docker load -i /tmp/docker-image.tar
 npm run dc:up
 npm run wait-4-docker
 
-npm run test:int || INTEGRATION_TEST_EXIT_CODE="$?"
-echo "==> integration tests exited with code: $INTEGRATION_TEST_EXIT_CODE"
+EXIT_CODE=0
+npm run test:int || EXIT_CODE="$?"
+echo "==> integration tests exited with code: $EXIT_CODE"
 
-docker-compose down
-exit $INTEGRATION_TEST_EXIT_CODE
+npm run dc:down
+exit $EXIT_CODE
